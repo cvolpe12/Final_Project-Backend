00e88d6edb25001e232fea9e44e3740eb96268fa
cvolpe12 <christopher.volpe12@gmail.com>
2019-04-15 12:34:03 -0400

    added extra_player boolean for player

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,4 +1,13 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :league_id, :entered
+  attributes :id, :user_id, :league_id, :entered, :players
   has_many :players
+  # has_many :drafts
+  def players
+    # byebug
+    self.object.drafts.map do |draft|
+      new_player = draft.player.attributes
+      new_player[:extra_player] = draft.extra_player
+      new_player
+    end
+  end
 end
